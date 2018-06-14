8b34c467be8e1f0597f006e1c951934d531dc000
Kerry Buckley <kerryjbuckley@gmail.com>
2018-06-14 20:51:18 +0100

    Display a photo

--- Picshare.elm
+++ Picshare.elm
@@ -1,9 +1,21 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, text, h1)
-import Html.Attributes exposing (class)
+import Html exposing (Html, div, img, h1, h2, text)
+import Html.Attributes exposing (class, src)
 
 
 main : Html msg
 main =
-    div [ class "header" ] [ h1 [] [ text "Picshare" ] ]
+    div []
+        [ div [ class "header" ]
+            [ h1 [] [ text "Picshare" ]
+            ]
+        , div [ class "content-flow" ]
+            [ div [ class "detailed-photo" ]
+                [ img [ src "https://programming-elm.com/1.jpg" ] []
+                , div [ class "photo-info" ]
+                    [ h2 [ class "caption" ] [ text "Surfing" ]
+                    ]
+                ]
+            ]
+        ]
