5627e9f39a20a4e89bafaae436e52b4b5ceb5f09
Kerry Buckley <kerryjbuckley@gmail.com>
2018-06-14 20:41:44 +0100

    Add a class to the header

--- Picshare.elm
+++ Picshare.elm
@@ -1,8 +1,9 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, text, h1)
+import Html.Attributes exposing (class)
 
 
 main : Html msg
 main =
-    div [] [ text "Picshare" ]
+    div [ class "header" ] [ h1 [] [ text "Picshare" ] ]
