a4c4d70d713117ec759ee7b8e20d688877f443cf
Kerry Buckley <kerryjbuckley@gmail.com>
2018-06-15 19:31:34 +0100

    Implement like button

--- Picshare.elm
+++ Picshare.elm
@@ -1,22 +1,46 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, img, h1, h2, text)
+import Html exposing (Html, beginnerProgram, div, i, img, h1, h2, text)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+main : Program Never { url : String, caption : String, liked : Bool } Msg
 main =
-    view initialModel
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
 
 
-initialModel : { url : String, caption : String }
+type Msg
+    = Like
+    | Unlike
+
+
+initialModel : { url : String, caption : String, liked : Bool }
 initialModel =
     { url = baseUrl ++ "1.jpg"
     , caption = "Surfer"
+    , liked = False
     }
 
 
-view : { url : String, caption : String } -> Html msg
+update :
+    Msg
+    -> { url : String, caption : String, liked : Bool }
+    -> { url : String, caption : String, liked : Bool }
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+view : { url : String, caption : String, liked : Bool } -> Html Msg
 view model =
     div []
         [ div [ class "header" ]
@@ -28,14 +52,30 @@ view model =
         ]
 
 
-viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
 viewDetailedPhoto model =
-    div [ class "detailed-photo" ]
-        [ img [ src model.url ] []
-        , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text model.caption ]
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+            else
+                Like
+    in
+        div [ class "detailed-photo" ]
+            [ img [ src model.url ] []
+            , div [ class "photo-info" ]
+                [ div [ class "like-button" ]
+                    [ i [ class "fa fa-2x", class buttonClass, onClick msg ] []
+                    ]
+                , h2 [ class "caption" ] [ text model.caption ]
+                ]
             ]
-        ]
 
 
 baseUrl : String
