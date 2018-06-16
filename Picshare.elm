3898c0311cf7d0c97e04d67d75e963294dac3788
Kerry Buckley <kerryjbuckley@gmail.com>
2018-06-16 17:27:29 +0100

    Tweak types ready to accept photos from Json API

--- Picshare.elm
+++ Picshare.elm
@@ -14,13 +14,22 @@ main =
         }
 
 
-type alias Model =
-    { url : String
+type alias Id =
+    Int
+
+
+type alias Photo =
+    { id : Id
+    , url : String
     , caption : String
     , liked : Bool
     , comments : List String
     , newComment : String
     }
+
+
+type alias Model =
+    Photo
 
 
 type Msg
@@ -31,7 +40,8 @@ type Msg
 
 initialModel : Model
 initialModel =
-    { url = baseUrl ++ "1.jpg"
+    { id = 1
+    , url = baseUrl ++ "1.jpg"
     , caption = "Surfer"
     , liked = False
     , comments = []
